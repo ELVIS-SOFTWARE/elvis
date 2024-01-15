7bf2902a623022dc3e85c9db1f542ba4fca948ac
dim145 <ddubois@sixmon.net>
2024-01-15 12:29:55 +0100

    fix: ELVIS-986 Problème génération site en 2.02

--- config/initializers/30-elvis.rb
+++ config/initializers/30-elvis.rb
@@ -14,12 +14,18 @@ Rails.application.config.after_initialize do
     PluginInitJob.perform_now
   end
 
-  unless Parameter.find_by(label: "app.clean_big_tables.max_nb_lines")
-    Parameter.create(label: "app.clean_big_tables.max_nb_lines", value: "300000", value_type: "int")
-  end
+  begin
+    unless Parameter.find_by(label: "app.clean_big_tables.max_nb_lines")
+      Parameter.create(label: "app.clean_big_tables.max_nb_lines", value: "300000", value_type: "int")
+    end
 
-  unless Parameter.find_by(label: "app.clean_big_tables.tables_to_clean")
-    Parameter.create(label: "app.clean_big_tables.tables_to_clean", value: "event_store_events,error_histories", value_type: "string")
+    unless Parameter.find_by(label: "app.clean_big_tables.tables_to_clean")
+      Parameter.create(label: "app.clean_big_tables.tables_to_clean", value: "event_store_events,error_histories", value_type: "string")
+    end
+  rescue ActiveRecord::NoDatabaseError => e
+    # ignore this error for init script
+  rescue StandardError => e
+    pp e
   end
 end
 
