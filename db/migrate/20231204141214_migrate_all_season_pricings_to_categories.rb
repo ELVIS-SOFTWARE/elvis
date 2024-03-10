d5157c0119af5c066718c112cec48a6cab469152
xavier <xavier@orzone.et>
2024-03-10 12:01:01 +0100

    fix: migrate pricings to pricing categories

--- db/migrate/20231204141214_migrate_all_season_pricings_to_categories.rb
+++ db/migrate/20231204141214_migrate_all_season_pricings_to_categories.rb
@@ -1,19 +1,21 @@
 class MigrateAllSeasonPricingsToCategories < ActiveRecord::Migration[6.1]
   def up
-    if Object.const_defined?(:Pricing)
-      Pricing.all.each do |pricing|
-        pc = PricingCategory.create!(id: pricing.id, name: pricing.label, is_a_pack: false)
-        pricing.activity_ref_season_pricings.each do |season_pricing|
-          puts season_pricing.inspect
-          arp = ActivityRefPricing.new(activity_ref_id: season_pricing.activity_ref_id, price: season_pricing.price, pricing_category_id: pc.id, from_season_id: season_pricing.season_id, to_season_id: season_pricing.season_id)
-          arp.save! validate: false # on ignore les validations pour éviter les erreurs lorsque l'ActivityRef a été soft-deleted
-        end
+
+    pricings = ActiveRecord::Base.connection.execute("SELECT * FROM pricings")
+    pricings.each do |pricing|
+      pc = PricingCategory.create!(id: pricing['id'], name: pricing['label'], is_a_pack: false)
+      ActiveRecord::Base.connection.execute("SELECT * FROM activity_ref_season_pricings WHERE pricing_id = #{pricing['id']}").each do |season_pricing|
+        puts season_pricing.inspect
+        arp = ActivityRefPricing.new(activity_ref_id: season_pricing['activity_ref_id'], price: season_pricing['price'], pricing_category_id: pc.id, from_season_id: season_pricing['season_id'], to_season_id: season_pricing['season_id'])
+        arp.save! validate: false # on ignore les validations pour éviter les erreurs lorsque l'ActivityRef a été soft-deleted
       end
     end
+
   end
 
   def down
-    ActivityRefPricing.destroy_all if const_defined?(:ActivityRefPricing)
-    PricingCategory.destroy_all if const_defined?(:PricingCategory)
+      ActiveRecord::Base.connection.execute("DELETE FROM activity_ref_pricings")
+      ActiveRecord::Base.connection.execute("DELETE FROM pricing_categories")
   end
+
 end
