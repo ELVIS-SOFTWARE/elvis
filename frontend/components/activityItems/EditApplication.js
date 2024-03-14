173d0a17ed0ddeb6d8b2b097354541ec8e07599a
MarieSo <mgibon@sixmon.net>
2024-03-14 15:55:50 +0100

    feat(ZIGGY--843-Demande-inscription-modif) update composant react

--- frontend/components/activityItems/EditApplication.js
+++ frontend/components/activityItems/EditApplication.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {useState} from "react";
+import swal from "sweetalert2";
+import Modal from "react-modal";
 
-
-export default function EditApplication (props) {
-    // useState pour ouvrir la modal
-    const [isEditModalOpen, setIsEditModalOpen]= useState(false);
+export default function EditApplication(props) {
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
     function handleEditModal() {
         setIsEditModalOpen(!isEditModalOpen);
@@ -19,9 +19,49 @@ export default function EditApplication (props) {
                         fontWeight: "bold"
                     }}
                     onClick={() => handleEditModal()}
-            >Modifier</button>
+            >Modifier
+            </button>
+
+            <Modal
+                isOpen={isEditModalOpen}
+                onRequestClose={() => handleEditModal()}
+                className="modal-xl"
+                ariaHideApp={false}
+                contentLabel="Modifier votre inscription"
+                style={{
+                    content: {
+                        width: "700px" // Agrandir la modal à 700px
+                    }
+                }}
+            >
+                <h2 className="modal-header mb-5" style={{color: "#00334A"}}>
+                    Modifier votre demande d'inscription
+                </h2>
+                <div className="content">
+                    <input type="text" id="editInput" name="editInput"
+                           style={{
+                               width: "100%", // Ajuster la largeur à 100% pour occuper toute la largeur de la modal
+                               height: "200px",
+                               textAlign: "left"
+                           }}
+                           placeholder="Indiquer les modifications à nous communiquer (disponibilité, choix professeur)"
+                           wrap="soft" // Permettre le retour automatique du texte du placeholder
+                    />
+                </div>
+                <div className="d-flex justify-content-end mt-5">
+                    <button className="btn text-white"
+                            style={{
+                                backgroundColor: "#00334A",
+                                borderRadius: "8px",
+                                fontWeight: "bold"
+                            }}>
+                        Modifier
+                    </button>
+                </div>
+
+            </Modal>
 
         </div>
 
-        )
+    )
 }
