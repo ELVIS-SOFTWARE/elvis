f7911ad9a1ebede7aac446646211c242047fbb18
MarieSo <mgibon@sixmon.net>
2024-03-14 16:55:41 +0100

    feat(ZIGGY--843-Demande-inscription-annul) création modal de confirmation

--- frontend/components/activityItems/EditApplication.js
+++ frontend/components/activityItems/EditApplication.js
@@ -5,9 +5,14 @@ import Modal from "react-modal";
 
 export default function EditApplication(props) {
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
 
     function handleEditModal() {
         setIsEditModalOpen(!isEditModalOpen);
+    }
+
+    function handleConfirmationModal() {
+        setIsConfirmationModalOpen(!isConfirmationModalOpen);
     }
 
     return (
@@ -25,33 +30,75 @@ export default function EditApplication(props) {
             <Modal
                 isOpen={isEditModalOpen}
                 onRequestClose={() => handleEditModal()}
-                className="modal-xl"
+                className="modal-xl fade"
                 ariaHideApp={false}
                 contentLabel="Modifier votre inscription"
                 style={{
                     content: {
-                        width: "700px" // Agrandir la modal à 700px
+                        width: "700px"
                     }
                 }}
             >
-                <h2 className="modal-header mb-5" style={{color: "#00334A"}}>
-                    Modifier votre demande d'inscription
-                </h2>
-                <div className="content" style={{height: "200px"}}>
-                    <textarea id="editInput" name="editInput" style={{width: "100%", height: "100%"}} wrap="soft">
-                        Indiquer les modifications à nous communiquer (disponibilité, choix professeur)
-                    </textarea>
+                <div className="m-5">
+                    <h2 className="modal-header mb-3" style={{color: "#00334A", textAlign: "left"}}>
+                        Modifier votre demande d'inscription
+                    </h2>
+                    <div className="content" style={{height: "200px"}}>
+                        <textarea id="editInput" name="editInput" style={{width: "100%", height: "100%"}} wrap="soft"
+                                  defaultValue="Indiquer les modifications à nous communiquer (disponibilité, choix professeur)"></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end mt-5">
+                        <button className="btn text-white"
+                                style={{
+                                    backgroundColor: "#00334A",
+                                    borderRadius: "8px",
+                                    fontWeight: "bold"
+                                }}
+                                onClick={() => {
+                                    handleEditModal();
+                                    handleConfirmationModal();
+                                }}
+
+                        >
+                            Modifier
+                        </button>
+                    </div>
                 </div>
-                <div className="d-flex justify-content-end mt-5">
-                    <button className="btn text-white"
-                            style={{
-                                backgroundColor: "#00334A",
-                                borderRadius: "8px",
-                                fontWeight: "bold"
-                            }}>
-                        Modifier
-                    </button>
+
+            </Modal>
+
+            <Modal
+                isOpen={isConfirmationModalOpen}
+                onRequestClose={() => handleConfirmationModal()}
+                className="modal-xl fade"
+                ariaHideApp={false}
+                contentLabel="Modification réussie"
+                style={{
+                    content: {
+                        width: "700px"
+                    }
+                }}
+            >
+                <div className="m-5">
+                    <h2 className="modal-header mb-3" style={{color: "#00334A", textAlign: "left"}}>
+                        Votre demande d'inscription a bien été modifiée
+                    </h2>
+                    <p className="h4 mb-5">Nous avons bien pris en compte les modifications de votre demande
+                        d'inscription.</p>
+                    <div className="d-flex justify-content-end mt-5">
+                        <button className="btn text-white"
+                                style={{
+                                    backgroundColor: "#00334A",
+                                    borderRadius: "8px",
+                                    fontWeight: "bold"
+                                }}
+                        onClick={() => handleConfirmationModal()}
+                        >
+                            Voir mes demandes
+                        </button>
+                    </div>
                 </div>
+
 
             </Modal>
 
