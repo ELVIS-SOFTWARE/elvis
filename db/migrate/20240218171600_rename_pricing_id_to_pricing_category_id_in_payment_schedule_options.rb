9aacfcec811d3573f20bbb62502b0a4a6ac97e0c
dim145 <ddubois@sixmon.net>
2024-03-21 11:09:26 +0100

    fix: constraint failed because null (update new table with old table values if exist)

--- db/migrate/20240218171600_rename_pricing_id_to_pricing_category_id_in_payment_schedule_options.rb
+++ db/migrate/20240218171600_rename_pricing_id_to_pricing_category_id_in_payment_schedule_options.rb
@@ -4,6 +4,9 @@ class RenamePricingIdToPricingCategoryIdInPaymentScheduleOptions < ActiveRecord:
 
     if column_exists?(:payment_schedule_options, :pricing_id)
       rename_column :payment_schedule_options, :pricing_id, :pricing_category_id
+
+      execute("insert into pricing_categories (name, number_lessons, is_a_pack, created_at, updated_at) select label as name, 31 as number_lessons, false as is_a_pack, now() as created_at, now() as updated_at from pricings order by id asc") if table_exists?(:pricings)
+
       add_foreign_key :payment_schedule_options, :pricing_categories, column: :pricing_category_id
     else
       add_reference :payment_schedule_options, :pricing_category, foreign_key: true
