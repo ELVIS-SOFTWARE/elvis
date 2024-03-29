3fbaa27e6c9ad5c5b239622e58fc445a9627fd68
dim145 <ddubois@sixmon.net>
2024-03-29 15:16:01 +0100

    feat: add reset password btn

---
+++ frontend/components/utils/BtnApiElement.js
@@ -0,0 +1,55 @@
+import React from "react";
+import * as api from "../../tools/api";
+import swal from "sweetalert2";
+import PropTypes from "prop-types";
+
+export default function BtnApiElement({ sendRequest, text, className, textError, textSuccess, textNoData })
+{
+    function onClick()
+    {
+        api.set()
+            .success((data) => {
+
+                if((!data || data.length === 0) && textNoData)
+                {
+                    swal({
+                        title: "error",
+                        type: "error",
+                        text: textNoData
+                    });
+                    return;
+                }
+
+                swal({
+                    title: "Email envoyé",
+                    type: "success",
+                    text: textSuccess
+                });
+            })
+            .error((res) => {
+                swal({
+                    title: "error",
+                    type: "error",
+                    text: textError || "Une erreur est survenue."
+                });
+            })[sendRequest.type](sendRequest.url, sendRequest.data, sendRequest.additionnalHeaders);
+    }
+
+    return <button onClick={onClick} className={className}>
+        {text}
+    </button>
+}
+
+BtnApiElement.propTypes = {
+    sendRequest: PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        data: PropTypes.object,
+        additionnalHeaders: PropTypes.object
+    }),
+    text: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
+    textError: PropTypes.string,
+    textSuccess: PropTypes.string,
+    textNoData: PropTypes.string
+};
