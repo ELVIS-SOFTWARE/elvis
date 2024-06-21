143994bc9b50eec2e93372d7f20e9d05de78ba77
dim145 <ddubois@sixmon.net>
2024-06-21 10:40:37 +0200

    fix: identification number not appear in user profile if not required

--- frontend/components/userForm/UserForm.js
+++ frontend/components/userForm/UserForm.js
@@ -315,7 +315,7 @@ class UserForm extends React.PureComponent {
                                         displayBirthday
                                         ignoreValidate={user ? user.is_admin : false}
                                         displayGender
-                                        displayIdentificationNumber={this.props.displayIdentificationNumber && this.state.requireIdentificationNumber}
+                                        displayIdentificationNumber={this.props.displayIdentificationNumber || this.state.requireIdentificationNumber}
                                         requireIdentificationNumber={this.state.requireIdentificationNumber}
                                         birthday={values.birthday}
                                         organizationOptions={this.props.organizationOptions}
