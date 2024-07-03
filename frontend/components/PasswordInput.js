fa94b961ee1863f71127765bb785bb8e82b149db
MarieSo <mgibon@sixmon.net>
2024-07-03 15:13:23 +0200

    feat (ELVIS-1182) retour PR

--- frontend/components/PasswordInput.js
+++ frontend/components/PasswordInput.js
@@ -1,21 +1,23 @@
+import React, {useState} from 'react';
 
-import React, { useState } from 'react';
-
-export default function PasswordInput ({ id, name, label, error, info }) {
+export default function PasswordInput({id, name, label, error, additional_attr}) {
     const [passwordVisible, setPasswordVisible] = useState(false);
+    const {minLength} = additional_attr || {};
+
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
     };
 
     return (
         <div className="form-group text-left">
-            <label htmlFor={id}>{label}</label><br />
+            <label htmlFor={id}>{label}</label><br/>
             <div className="d-inline-flex w-100">
                 <input
                     type={passwordVisible ? "text" : "password"}
                     className="form-control"
                     id={id}
                     name={name}
+                    minLength={minLength}
                 />
                 <button
                     type="button"
@@ -25,7 +27,7 @@ export default function PasswordInput ({ id, name, label, error, info }) {
                     <i className={`fas fa-eye${passwordVisible ? '-slash' : ''}`}></i>
                 </button>
             </div>
-            {info && <p className="form-text text-muted">({info} caractères minimum)</p> }
+            {minLength && <p className="form-text text-muted">({minLength} caractères minimum)</p>}
             {error && <div className="alert alert-danger">{error}</div>}
         </div>
     );
