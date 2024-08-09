cd85cea1d6d6ba410b2b5c3254dd3384ad7690cf
MarieSo <mgibon@sixmon.net>
2024-08-09 14:45:49 +0200

    feat (ELVIS-1211) retour PR

--- frontend/components/ActivitiesApplicationsList.js
+++ frontend/components/ActivitiesApplicationsList.js
@@ -405,11 +405,11 @@ class ActivitiesApplicationsList extends React.Component {
         if (this.state.bulkTargets === "all") {
             return true;
         }
-            const statusFilter = [...this.state.data]
-                .filter(f => (this.state.bulkTargets || []).includes(f.id))
-                .map(f => f.activity_application_status_id);
+        const statusFilter = [...this.state.data]
+            .filter(f => (this.state.bulkTargets || []).includes(f.id))
+            .map(f => f.activity_application_status_id);
 
-            return statusFilter.some(s => [ACTIVITY_ATTRIBUTED_ID, ACTIVITY_PROPOSED_ID, PROPOSAL_ACCEPTED_ID].includes(s));
+        return statusFilter.some(s => [ACTIVITY_ATTRIBUTED_ID, ACTIVITY_PROPOSED_ID, PROPOSAL_ACCEPTED_ID].includes(s));
 
     }
 
