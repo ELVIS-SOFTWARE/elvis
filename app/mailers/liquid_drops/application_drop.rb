c33fe309a73387e1862a0198d67767bd810e94bf
MarieSo <mgibon@sixmon.net>
2024-09-24 12:21:40 +0200

    feat (ELVIS-1238) fixed

--- app/mailers/liquid_drops/application_drop.rb
+++ app/mailers/liquid_drops/application_drop.rb
@@ -47,7 +47,16 @@ module LiquidDrops
     end
 
     def total_due_payments
-      "woop woop"
+      user = @application["user"]
+      payment_schedules = user["payment_schedules"] || []
+
+      due_payments = payment_schedules.map { |schedule| schedule["due_payments"] }.flatten
+      pending_due_payments = due_payments.select do |dp|
+        dp["due_payment_status_id"] != DuePaymentStatus::PAID_ID
+      end
+      total_due = pending_due_payments.sum { |dp| dp["amount"].to_f }
+
+      total_due
     end
 
   end
