7084f7cc7aedb7c0631824f900aadc2b2972a60f
dim145 <dimitri.bleach@gmail.com>
2024-11-19 17:55:18 +0100

    feat: adapt configs to use old structs and new compilator

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,17 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 const { generateWebpackConfig } = require('shakapacker')
 
-const webpackConfig = generateWebpackConfig()
+const options = {
+    resolve: {
+        extensions: ['.css', '.scss', '.js', '.jsx'],
+        alias: {
+            jquery: 'jquery/src/jquery',
+            React: 'react',
+            ReactDOM: 'react-dom',
+        }
+    }
+}
+
+const webpackConfig = generateWebpackConfig(options)
 
 module.exports = webpackConfig
