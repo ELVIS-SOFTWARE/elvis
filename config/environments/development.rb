5487d36b2903f7726c9d479e93b913b3736a05bb
dim145 <dimitri.bleach@gmail.com>
2024-11-20 15:29:19 +0100

    refactor: remove useless comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  #config.webpacker.check_yarn_integrity = false  # Settings specified here will take precedence over those in config/application.rb.
-
   config.web_console.permissions = "0.0.0.0/0"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
