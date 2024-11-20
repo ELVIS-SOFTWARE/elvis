e3763f7019bb8d09f443483b7980f1c7f770a96d
dim145 <dimitri.bleach@gmail.com>
2024-11-20 15:00:28 +0100

    fix: asset path error

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -5,8 +5,18 @@ const webpack = require("webpack");
 const options = {
     resolve: {
         extensions: ['.css', '.scss', '.js', '.jsx'],
-
-    }
+        alias: {
+            jquery: 'jquery/src/jquery',
+            React: 'react',
+            ReactDOM: 'react-dom',
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+        }),
+    ],
 }
 
 const webpackConfig = generateWebpackConfig(options)
