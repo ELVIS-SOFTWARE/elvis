e4ca7fcd76973c51d030ac236912ea0c652c2d64
dim145 <dimitri.bleach@gmail.com>
2024-12-16 17:30:03 +0100

    fix: use closest_instance_from_now in my_activities page (same as user page)

--- frontend/components/PacksApplications/MyActivities/regularActivityCards.jsx
+++ frontend/components/PacksApplications/MyActivities/regularActivityCards.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import moment from "moment";
 import 'moment/locale/fr';
 import packImgDefault from "../../../images/default_activity.png";
+import _ from "lodash";
 
 moment.locale('fr');
 
@@ -16,6 +17,9 @@ export default function regularActivityCards(props) {
                 const activity = desired_activity.activity;
                 const activity_ref = desired_activity.activity_ref;
 
+                const start = _.get(activity, 'closest_instance_from_now.start', null) || _.get(activity, 'time_interval.start', null);
+                const end = _.get(activity, 'closest_instance_from_now.end', null) || _.get(activity, 'time_interval.end', null);
+
                 return <div className="card activity-card my-3 mx-3">
                     <div className="card-img-wrapper">
                         <img className="card-img-packs"
@@ -29,7 +33,7 @@ export default function regularActivityCards(props) {
                         <h4 className="card-title pl-4 pt-3" style={{ color: '#00283B' }}>{activity_ref.label}</h4>
                         <p className="card-text pl-4" style={{ color: '#00334A' }}>{activity.room.label}</p>
                         <p className="card-text pl-4">
-                            {moment(activity.time_interval.start).format('dddd') + " | " + moment(activity.time_interval.start).format('HH:mm') + " - " + moment(activity.time_interval.end).format('HH:mm')}
+                            {moment(start).format('dddd') + " | " + moment(start).format('HH:mm') + " - " + moment(end).format('HH:mm')}
                         </p>
                         <div className="d-flex align-items-center pl-3">
                             <img className="rounded-circle"
