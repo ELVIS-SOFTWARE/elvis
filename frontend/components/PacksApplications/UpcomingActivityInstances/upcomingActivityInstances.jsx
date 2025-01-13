2b601b1fecb89ca685cc6231c398aa301c68342e
dim145 <dimitri.bleach@gmail.com>
2025-01-13 16:12:08 +0100

    fix: undefined user_id in upcoming lesson list (user view)

--- frontend/components/PacksApplications/UpcomingActivityInstances/upcomingActivityInstances.jsx
+++ frontend/components/PacksApplications/UpcomingActivityInstances/upcomingActivityInstances.jsx
@@ -1,14 +1,14 @@
 import React, {Fragment, useEffect, useState} from "react";
 import UpcomingActivityInstancesList from "./upcomingActivityInstancesList";
 
-export default function upcomingActivityInstances() {
+export default function upcomingActivityInstances({user_id}) {
     return (
         <div className="d-flex flex-column ">
             <div className="mt-4">
                 <h3 className="title font-bold">MON PLANNING</h3>
             </div>
             <div className="div-scrollable">
-                <UpcomingActivityInstancesList/>
+                <UpcomingActivityInstancesList user_id={user_id}/>
             </div>
         </div>
     );
