40e1f74b5bf0b19f58ff1f2e4977d9bf6c3574f2
Raphael Rault <ludira80@gmail.com>
2025-02-17 14:43:52 +0100

    add: Option indicator

--- frontend/components/courses/LessonList.jsx
+++ frontend/components/courses/LessonList.jsx
@@ -1036,12 +1036,6 @@ export default class LessonList extends React.Component {
                                 rowInfo &&
                                 rowInfo.original.options.length != 0
                             ) {
-                                return {
-                                    style: {
-                                        color: "#9575CD",
-                                        fontWeight: "bold",
-                                    },
-                                };
                             }
                             return {};
                         }}
