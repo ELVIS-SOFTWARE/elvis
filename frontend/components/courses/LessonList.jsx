1025e6a5b1699fa057cc98df41ffd3414c74d509
Raphael Rault <ludira80@gmail.com>
2025-02-27 16:03:21 +0100

    add: name "action"

--- frontend/components/courses/LessonList.jsx
+++ frontend/components/courses/LessonList.jsx
@@ -891,7 +891,7 @@ export default class LessonList extends React.Component {
                 ),
             },
             {
-                Header: "",
+                Header: "Action",
                 filterable: false,
                 sortable: false,
                 Cell: c => (
